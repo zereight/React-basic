8714382dc944ba2a38a9d1b346068a350d178ce6
zereight <aldwk68@naver.com>
2019-04-11 21:24:57 +0900

    6. State practice 2

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -9,24 +9,7 @@ class App extends Component {
 
   state = { 
     greeting : `Hello`,
-    movies : [
-      {
-        title: "Get Out",
-        poster: "https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_.jpg"
-      },
-      {
-        title: "Money",
-        poster: "hhttp://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81651/81651_1000.jpg"
-      },
-      {
-        title: "Avengets",
-        poster: "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/10/1520596571-avengers-art.jpg"
-      },
-      {
-        title: "Venom",
-        poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000080/80589/80589_1000.jpg" 
-      }
-    ]
+    movies : []
   };
 
 
@@ -39,6 +22,22 @@ class App extends Component {
       this.setState({
         movies : [
           ...this.state.movies, // ... add on remained movies
+          {
+            title: "Get Out",
+            poster: "https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_.jpg"
+          },
+          {
+            title: "Money",
+            poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81651/81651_1000.jpg"
+          },
+          {
+            title: "Avengets",
+            poster: "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/10/1520596571-avengers-art.jpg"
+          },
+          {
+            title: "Venom",
+            poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000080/80589/80589_1000.jpg" 
+          },
           { //new movies
             title: "Bumble Bee",
             poster: "https://cdn-images-1.medium.com/max/2600/1*asCuPasrO5qPB4hAAu2upQ.jpeg"
@@ -48,13 +47,17 @@ class App extends Component {
     },1000); //after 1 sec
   }
 
+ _renderMovies = () =>  {
+  return this.state.movies.map( movie => {
+    return (<Movie title={movie.title} poster={movie.poster} />);
+  }) ;
+}
+
   render() {
     return (
       <div className="App">
           <h5>{this.state.greeting}</h5>
-          {this.state.movies.map( movie => {
-            return (<Movie title={movie.title} poster={movie.poster} />);
-        } )}      
+          {this.state.movies.length !== 0 ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
