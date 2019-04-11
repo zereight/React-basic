eaba4e879375968364d6932bbb821288faa6f69d
zereight <aldwk68@naver.com>
2019-04-11 20:36:34 +0900

    5. State practice 1

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -3,29 +3,30 @@ import './App.css';
 import Movie from "./movie";
 
 
-const movies = [
-  {
-    title: "Get Out",
-    poster: "https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_.jpg"
-  },
-  {
-    title: "Money",
-    poster: "hhttp://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81651/81651_1000.jpg"
-  },
-  {
-    title: "Avengets",
-    poster: "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/10/1520596571-avengers-art.jpg"
-  },
-  {
-    title: "Venom",
-    poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000080/80589/80589_1000.jpg" 
-  }
-];
+
 
 class App extends Component {
 
-  state = { //component state is Dynamic Variable for components, It expresses Component's  state
-    greeting : `Hello`
+  state = { 
+    greeting : `Hello`,
+    movies : [
+      {
+        title: "Get Out",
+        poster: "https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_.jpg"
+      },
+      {
+        title: "Money",
+        poster: "hhttp://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81651/81651_1000.jpg"
+      },
+      {
+        title: "Avengets",
+        poster: "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/10/1520596571-avengers-art.jpg"
+      },
+      {
+        title: "Venom",
+        poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000080/80589/80589_1000.jpg" 
+      }
+    ]
   };
 
 
@@ -33,22 +34,26 @@ class App extends Component {
 
   }
 
-  componentDidMount(){ // working after rendering
-    setTimeout( () => {
-      this.setState(      // you must change state's value using setState(), because it can call component's Life cycle function.
-        {
-          greeting: `Hello twice!`
-        }
-      )
-    }, 1000 );
+  componentDidMount(){ 
+    setTimeout(() => {
+      this.setState({
+        movies : [
+          ...this.state.movies, // ... add on remained movies
+          { //new movies
+            title: "Bumble Bee",
+            poster: "https://cdn-images-1.medium.com/max/2600/1*asCuPasrO5qPB4hAAu2upQ.jpeg"
+          }
+        ]
+      });
+    },1000); //after 1 sec
   }
 
   render() {
     return (
       <div className="App">
           <h5>{this.state.greeting}</h5>
-          {movies.map( movie => {
-          return (<Movie title={movie.title} poster={movie.poster} />);
+          {this.state.movies.map( movie => {
+            return (<Movie title={movie.title} poster={movie.poster} />);
         } )}      
       </div>
     );
