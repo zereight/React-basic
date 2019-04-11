59561db256b533debfdf9c70efaa806d98ff9949
zereight <aldwk68@naver.com>
2019-04-11 19:45:41 +0900

    2. Props

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movie from "./movie";
+const movieTitles = [
+  "Get Out",
+  "Money",
+  "Avengets",
+  "Venom"
+];
+
+const movieImages = [
+  "https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_.jpg",
+  "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81651/81651_1000.jpg",
+  "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/10/1520596571-avengers-art.jpg",
+  "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000080/80589/80589_1000.jpg"  
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie/>
+        <Movie title={movieTitles[0]} poster={movieImages[0]} />
+        <Movie title={movieTitles[1]} poster={movieImages[1]} />
+        <Movie title={movieTitles[2]} poster={movieImages[2]} />
+        <Movie title={movieTitles[3]} poster={movieImages[3]} />        
       </div>
     );
   }
--- movie_app/src/movie.js
+++ movie_app/src/movie.js
@@ -1,12 +1,15 @@
 import React, {Component} from "react";
 import "./movie.css";
 
+
+
 class Moive extends Component {
     render(){
         return (
             <div>
                 <h1>Hello guys!</h1>
-                <MoviePoster/>
+                <h2>{this.props.title}</h2>
+                <MoviePoster poster={this.props.poster}/>
             </div>
         );
     }
@@ -14,8 +17,9 @@ class Moive extends Component {
 
 class MoviePoster extends Component{
     render(){
+        //console.log(this.props.title);
         return(
-            <img src="https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2019/04/02/105828186-1554212544565avengers-endgame-poster-og-social-crop.530x298.jpg?v=1554212633" />
+            <img alt="movie images" src={this.props.poster} />
         );
     }
 }
