c9e40fe0d365cb5f670480ee0ffb11abee7a0988
zereight <aldwk68@naver.com>
2019-04-11 19:53:28 +0900

    3. Using the map function

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movie from "./movie";
-const movieTitles = [
-  "Get Out",
-  "Money",
-  "Avengets",
-  "Venom"
-];
 
-const movieImages = [
-  "https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_.jpg",
-  "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81651/81651_1000.jpg",
-  "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/10/1520596571-avengers-art.jpg",
-  "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000080/80589/80589_1000.jpg"  
+
+const movies = [
+  {
+    title: "Get Out",
+    poster: "https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_.jpg"
+  },
+  {
+    title: "Money",
+    poster: "hhttp://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81651/81651_1000.jpg"
+  },
+  {
+    title: "Avengets",
+    poster: "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/10/1520596571-avengers-art.jpg"
+  },
+  {
+    title: "Venom",
+    poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000080/80589/80589_1000.jpg" 
+  }
 ];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]} />
-        <Movie title={movieTitles[1]} poster={movieImages[1]} />
-        <Movie title={movieTitles[2]} poster={movieImages[2]} />
-        <Movie title={movieTitles[3]} poster={movieImages[3]} />        
+        {movies.map( movie => {
+          return (<Movie title={movie.title} poster={movie.poster} />);
+        } )}      
       </div>
     );
   }
