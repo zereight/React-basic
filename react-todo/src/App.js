bcc0e6f5e6fbd5cda0f7624e1669b8e5e85365ff
zereight <fullstackmachinelearning@gmail.com>
2021-01-09 13:39:41 +0900

    init

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,41 +1,8 @@
 import React, { useState } from "react";
-import TodoForm from "./TodoInput";
+import TodoInput from "./TodoInput";
+import Todos from "./Todos"
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "test1",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      text: "test2",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      text: "test3",
-      isCompleted: false,
-    },
-  ]);
-
-  const addTodo = (text) => {
-    setTodos([...todos, { id: todos.length + 1, text, isCompleted: false }]);
-    console.log(todos)
-  };
-
-  const toggleComplete = (id) => {
-    const targetIndex = todos.findIndex((todo) => todo.id === id);
-    todos[targetIndex].isCompleted = !todos[targetIndex].isCompleted;
-    setTodos(todos);
-  };
-
-  const editTitle = (index, text) => {
-    todos[index].text = text;
-    setTodos(todos);
-  };
-
-  return <div className="App">{<TodoForm addTodo={addTodo} />}</div>;
+  
 }
 
 export default App;
